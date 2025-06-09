9a249267924e9c62d7ce2e6d31569a461c82ec44
Marcos Costa <marcos.datadev@gmail.com>
2025-06-09 11:34:50 -0300

    Parcial da aula

--- galeria/views.py
+++ galeria/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 
+dados = {
+    1: {'nome': 'Nebulosa de Carina',
+        'legenda': 'webbtelescope.org / Nasa / James Webb'},
+    2: {'nome': 'Galáxia NGC 1079',
+        'legenda': 'nasa.org / Nasa / Hubble'}
+}
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    return render(request,
+                  'galeria/index.html',
+                  {'cards': dados})
 
 def imagem(request):
-    return render(request, 'galeria/imagem.html')
+    return render(request,
+                  'galeria/imagem.html')
