8c50c3cc6630d0fbcf5738d0960e6a48b87bec9b
Marcos Costa <marcos.datadev@gmail.com>
2025-06-09 08:49:56 -0300

    Inclusão de imagens e arquivos estáticos

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('<h1>Alura Space</h1><p>Bem vindo ao Alura Space</p>')
+    return render(request, 'galeria/index.html')
